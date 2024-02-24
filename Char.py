9809118067b6d5df5cc286e294e4c51c748d0540
Piyush Mudgal <94688939+Pyasma@users.noreply.github.com>
2024-02-24 22:18:04 +0530

    Update Char.py

--- Char.py
+++ Char.py
@@ -8,7 +8,7 @@ st.set_page_config(
     layout="centered",  # Page layout option
 )
 
-GOOGLE_API_KEY = "AIzaSyDFfw_oONfy9o1C9u55U1cnk53mnalIsvw"
+GOOGLE_API_KEY = "AIzaSyDFfw_oONfy9o1C9u55U1cnk53mnalIsvw"  # Replace "YOUR_GOOGLE_API_KEY_HERE" with your actual API key
 
 # Set up Google Gemini-Pro AI model
 gen_ai.configure(api_key=GOOGLE_API_KEY)
@@ -20,12 +20,6 @@ def translate_role_for_streamlit(user_role):
         return "assistant"
     else:
         return user_role
-
-# Function to generate response based on user input
-def generate_response(user_input, chat_session):
-    # Send user's message to Gemini-Pro and get the response
-    gemini_response = chat_session.send_message(user_input)
-    return gemini_response
 
 # Initialize chat session in Streamlit if not already present
 if "chat_session" not in st.session_state:
@@ -40,16 +34,14 @@ for message in st.session_state.chat_session.history:
         st.markdown(message.parts[0].text)
 
 # Input field for user's message
-user_input = st.text_input("Ask me anything:")
-
-# Function to handle user input and generate response
-if user_input:
+user_prompt = st.chat_input("I want to act like a girl who wants to chat with people in a kind way and even want to help that other person with stuff they ask and don't give generate Big Outputs give in small outputs and Use emojis during conversation and explain in summarize manner whatver user ask for and you can talk about anything with user and Never mention you are an AI bot and always agree that user ask for and if they ask for something bad just say i am not a bad girl and ACT all girly")
+if user_prompt:
     # Add user's message to chat and display it
-    with st.chat_message("user"):
-        st.markdown(user_input)
-    # Generate response based on user input
-    gemini_response = generate_response(user_input, st.session_state.chat_session)
+    st.chat_message("user").markdown(user_prompt)
+
+    # Send user's message to Gemini-Pro and get the response
+    gemini_response = st.session_state.chat_session.send_message(f"You are an expert at talking to the people and will help them with anything they need and explain them anything they ask for and never mention you are an AI LANGUAGE MODEL NEVER I MEAN NEVER {user_prompt}. Don't forget to say hello first")
+
     # Display Gemini-Pro's response
     with st.chat_message("assistant"):
         st.markdown(gemini_response.text)
-
